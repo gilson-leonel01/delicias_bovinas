f0339bf110d0b2a826111dc3091288b2d3e64c2f
gilson-leonel01 <gilson.leonel05@gmail.com>
2025-11-23 19:45:52 +0100

    Added mockup info file

---
+++ frontend/src/mockups/faqsMockup.js
@@ -0,0 +1,18 @@
+export const faqsMockup = [
+  {
+    question: 'Como finalizar a compra?',
+    answer: 'Para finalizar sua compra, adicione os produtos ao carrinho, revise os itens e clique em "Finalizar Compra". Preencha seus dados de entrega e escolha a forma de pagamento. Após a confirmação, você receberá um email com os detalhes do pedido.'
+  },
+  {
+    question: 'Como fazer uma reserva?',
+    answer: 'Clique no botão "RESERVAR" na seção de produtos desejados. Preencha o formulário com suas informações e a data desejada. Nossa equipe entrará em contato para confirmar sua reserva.'
+  },
+  {
+    question: 'Como entrar em contato?',
+    answer: 'Você pode nos contatar através do formulário na seção de contatos, pelo email deliciasbovinas@gmail.com, telefone +244 928 826 987 / 923 456 789, ou visite nosso estabelecimento no Bairro Popular, Rua Do Kilabo.'
+  },
+  {
+    question: 'Como recuperar a senha?',
+    answer: 'Clique em "Esqueci minha senha" na página de login. Insira seu email cadastrado e você receberá um link para redefinir sua senha.'
+  }
+];
---
+++ frontend/src/mockups/productsMockup.js
@@ -0,0 +1,56 @@
+export const productsMockup = [
+  {
+    id: 1,
+    name: 'Vazia (Coxão Duro)',
+    category: 'Carne de Vaca',
+    price: 100000,
+    image: 'https://images.unsplash.com/photo-1603048588665-791ca8aea617?w=400&h=300&fit=crop',
+    rating: 5,
+    discount: 20
+  },
+  {
+    id: 2,
+    name: 'Vazia (Coxão Mole)',
+    category: 'Carne de Vaca',
+    price: 100000,
+    image: 'https://images.unsplash.com/photo-1607623814075-e51df1bdc82f?w=400&h=300&fit=crop',
+    rating: 5,
+    discount: 15
+  },
+  {
+    id: 3,
+    name: 'Lombo (Contra Filé)',
+    category: 'Carne de Vaca',
+    price: 100000,
+    image: 'https://images.unsplash.com/photo-1588347818036-4c3a5e8d0b02?w=400&h=300&fit=crop',
+    rating: 5,
+    discount: 5
+  },
+  {
+    id: 4,
+    name: 'Alcatra (Coxão)',
+    category: 'Carne de Vaca',
+    price: 100000,
+    image: 'https://images.unsplash.com/photo-1602470520998-f4a52199a3d6?w=400&h=300&fit=crop',
+    rating: 5,
+    discount: 10
+  },
+  {
+    id: 5,
+    name: 'Picanha Premium',
+    category: 'Carne de Vaca',
+    price: 100000,
+    image: 'https://images.unsplash.com/photo-1607623814075-e51df1bdc82f?w=400&h=300&fit=crop',
+    rating: 5,
+    discount: null
+  },
+  {
+    id: 6,
+    name: 'Filé Mignon',
+    category: 'Carne de Vaca',
+    price: 100000,
+    image: 'https://images.unsplash.com/photo-1603048588665-791ca8aea617?w=400&h=300&fit=crop',
+    rating: 5,
+    discount: null
+  }
+];
